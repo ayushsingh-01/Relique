151d5a0b2eb1024e67a47dc25f5906e6d01f6341
Alhamd <alhamdkhan016@gmail>
2026-05-18 21:16:07 +0530

    Enable SSL for database connections on Vercel

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA', isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) ? '/etc/pki/tls/certs/ca-bundle.crt' : null),
             ]) : [],
         ],
 
